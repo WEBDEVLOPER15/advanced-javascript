703df2d84ad5c4442476b3ba1061ba793267b2fd
WEBDEVLOPER15 <ashraful190043@gmail.com>
2021-02-02 11:13:22 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,36 @@
+// 0
+// "";false
+// variable defined na korlee False
+// null dilee false
+// let name =NaN; not a number false
+// Truthy
+// '0',''[]
+// const age = 4;
+
+// if(age){
+
+//     console.log("condition is true");
+// }
+// else{
+//     console.log("condition is false");
+// }
+// const name = "0";
+
+// let name;
+// console.log(name);
+// if(name ){
+
+//     console.log("condition is true");
+// }
+// else{
+//     console.log("condition is false");
+// }
+let name = 12;
+
+if(name || name == 0){
+
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
